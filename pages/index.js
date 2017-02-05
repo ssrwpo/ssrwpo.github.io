dbdf55ff2f276ca04fb3090e6c84d1fe70f22618
PEM-- <pemarchandet@gmail.com>
2017-02-05 08:37:23 +0100

    Ready for build

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default class Index extends React.Component {
         <h1>
           Hi people
         </h1>
-        <p>Welcome to your new Gatsby site</p>
+        <p>Welcome to SSRWPO site</p>
         <h2>Below are some pages showing different capabilities built-in to Gatsby</h2>
         <h3>Supported file types</h3>
         <ul>
